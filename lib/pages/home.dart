f67924b63119a5481c90d92286a2811155bce23b
Djameleddine21 <hd_sebbagh@esi.dz>
2020-01-06 01:51:10 +0100

    add search fonctionaity

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -7,29 +10,56 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  final String _url = " https://owlbot.info/api/v4/dictionary/owl";
+  final String _url = "https://owlbot.info/api/v4/dictionary/";
   final String _token = "11cf0bd189d735d354d2d9c4e30c54202757ebc8";
-
   TextEditingController _controller = TextEditingController();
+
+  StreamController _streamController;
+  Stream _stream;
+  Timer _debounce;
+
+  @override
+  initState() {
+    super.initState();
+    _streamController = StreamController();
+    _stream = _streamController.stream;
+  }
+
+  _search() async {
+    if (_controller.text == null || _controller.text.length == 0) {
+      _streamController.add(null);
+      return;
+    }
+    _streamController.add("attendre");
+    Response response =
+        await get(_url + _controller.text.trim(), headers: {"Authorization": "Token " + _token});
+    _streamController.add(json.decode(response.body));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text("Flutter Dictionary"),
         bottom: PreferredSize(
-          preferredSize: Size.fromHeight(45.0),
+          preferredSize: Size.fromHeight(50.0),
           child: Row(
             children: <Widget>[
               Expanded(
                 child: Container(
-                  margin: const EdgeInsets.only(left: 12.0,bottom: 8.0),
+                  margin: const EdgeInsets.only(left: 10.0, bottom: 8.0),
                   decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20.0,)
-                  ),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(
+                        20.0,
+                      )),
                   child: TextFormField(
-                    onChanged: (String text){
+                    onChanged: (String text) {
+                      if(_debounce?.isActive ?? false ) _debounce.cancel();
+                      _debounce = Timer(const Duration(milliseconds: 1000),(){
+                        _search();
+                      });
                     },
                     controller: _controller,
                     decoration: InputDecoration(
@@ -41,16 +71,60 @@ class _HomeState extends State<Home> {
                 ),
               ),
               IconButton(
-                icon: Icon(Icons.search,color: Colors.white,),
-                onPressed: (){
-
-                },
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                onPressed: _search,
               )
             ],
           ),
         ),
       ),
-      
+      body: Container(
+        margin: EdgeInsets.all(8.0),
+        child: StreamBuilder(
+          stream: _stream,
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            if (snapshot.data == null) {
+              return Center(
+                child: Text("Enter a search word"),
+              );
+            }
+            if(snapshot.data=="attendre"){
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return ListView.builder(
+              itemCount: snapshot.data["definitions"].length,
+              itemBuilder: (BuildContext context, int index){
+                return ListBody(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.grey[350],
+                      ),
+                      child: ListTile(
+                        leading: snapshot.data["definitions"][index]["image_url"]==null ? null : CircleAvatar(
+                          backgroundImage: NetworkImage(snapshot.data["definitions"][index]["image_url"]),
+                        ),
+                        title: Text(_controller.text.trim()+'('+ snapshot.data["definitions"][index]["type"]+') :'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(snapshot.data["definitions"][index]["definition"]),
+                    )
+                  ],
+                );
+              },
+            );
+          },
+        ),
+      ),
     );
   }
 }
